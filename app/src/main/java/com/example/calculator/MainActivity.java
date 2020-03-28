894a8efb0a1f6cf3c48242e68b96837952e357f5
Joanna Palewicz <joannap@qualtrics.com>
2020-03-28 13:32:41 +0100

    Basic calculator version

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.calculator;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,11 +12,24 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.IBinder;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText firstNumberInput;
+    private EditText secondNumberInput;
+    private EditText resultEditText;
+    private Button addButton;
+    private Button subButton;
+    private Button mulButton;
+    private Button divButton;
+    private Button piButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +38,85 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        // Binding
+        firstNumberInput = (EditText) findViewById(R.id.firstNumberInput);
+        secondNumberInput = (EditText) findViewById(R.id.secondNumberInput);
+        resultEditText = (EditText) findViewById(R.id.resultEditText);
+
+        addButton = (Button) findViewById(R.id.addButton);
+        subButton = (Button) findViewById(R.id.subButton);
+        mulButton = (Button) findViewById(R.id.mulButton);
+        divButton = (Button) findViewById(R.id.divButton);
+        piButton = (Button) findViewById(R.id.piButton);
+
+        // Handle actions
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (mBound) {
+                    Double value1 = getValueFromInput(firstNumberInput);
+                    Double value2 = getValueFromInput(secondNumberInput);
+
+                    if (value1 != null && value2 != null) {
+                        double result = logicService.add(value1, value2);
+                        setResult(result);
+                    }
+                }
             }
         });
+
+        subButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (mBound) {
+                    Double value1 = getValueFromInput(firstNumberInput);
+                    Double value2 = getValueFromInput(secondNumberInput);
+
+                    if (value1 != null && value2 != null) {
+                        double result = logicService.sub(value1, value2);
+                        setResult(result);
+                    }
+                }
+            }
+        });
+
+        divButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (mBound) {
+                    Double value1 = getValueFromInput(firstNumberInput);
+                    Double value2 = getValueFromInput(secondNumberInput);
+
+                    if (value1 != null && value2 != null) {
+                        double result = logicService.div(value1, value2);
+                        setResult(result);
+                    }
+                }
+            }
+        });
+
+        mulButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (mBound) {
+                    Double value1 = getValueFromInput(firstNumberInput);
+                    Double value2 = getValueFromInput(secondNumberInput);
+
+                    if (value1 != null && value2 != null) {
+                        double result = logicService.mul(value1, value2);
+                        setResult(result);
+                    }
+                }
+            }
+        });
+    }
+
+    private Double getValueFromInput(EditText editTextInput){
+        String inputTextValue = editTextInput.getText().toString();
+        if (inputTextValue.equals("")){
+            return null;
+        }
+        return Double.parseDouble(inputTextValue);
+    }
+
+    private void setResult(double result) {
+        resultEditText.setText(Double.toString(result));
     }
 
     @Override
@@ -51,5 +139,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    LogicService logicService;
+    boolean mBound = false;
+    private ServiceConnection logicConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            LogicService.LocalBinder binder = (LogicService.LocalBinder) service;
+            logicService = binder.getService();
+            mBound = true;
+            Toast.makeText(MainActivity.this, "Logic Service Connected!",
+                    Toast.LENGTH_SHORT).show();
+        }
+        public void onServiceDisconnected(ComponentName className) {
+            logicService = null;
+            mBound = false;
+            Toast.makeText(MainActivity.this, "Logic Service Disconnected!",
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mBound) {
+            this.bindService(new Intent(MainActivity.this,LogicService.class),
+                    logicConnection, Context.BIND_AUTO_CREATE);
+        }
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBound) {
+            mBound = false;
+            this.unbindService(logicConnection);
+        }
     }
 }
